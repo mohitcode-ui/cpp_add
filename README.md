e5b7fe2cbcb9fc7dcdf7038e4959b3b7e0520d21
Mohit <mohitsingh11082002@gmail.com>
2020-09-30 16:49:45 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+sum in cpp
---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int i, j;
+    cin >> i >> j;
+    cout << i-j << endl;
+    
+    return 0;
+}
